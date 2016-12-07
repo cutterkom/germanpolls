27d76b6fdc7c4180678bd968777485e7f94cbcc3
Katharina Brunner <katharina.brunner@sueddeutsche.de>
2016-12-07 11:44:39 +0100

    Add new, better scraper

--- R/scrape_wahlrechtde.R
+++ R/scrape_wahlrechtde.R
@@ -1,64 +1,102 @@
-#' A function to scrape Wahlrecht.de
-#' 
-#' germanpolls() is a function that gets polling data from [Wahlrecht.de](http://www.wahlrecht.de) for seven polling organisations and ten parties.
-#' @importFrom magrittr %>%
-#' @importFrom rvest html_table
-#' @importFrom xml2 read_html
-#' @importFrom dplyr filter
-#' @importFrom dplyr rename
-#' @importFrom dplyr mutate
-#' @importFrom dplyr select
-#' @importFrom purrr map
-#' @importFrom purrr map_at
-#' 
-#' @examples
-#' polling_data <- germanpolls() 
-#' 
-#' @export
-#'
+  #' A function to scrape Wahlrecht.de
+  #' 
+  #' germanpolls() is a function that gets polling data from [Wahlrecht.de](http://www.wahlrecht.de) for seven polling organisations and ten parties.
+  #' @importFrom magrittr %>%
+  #' @importFrom rvest html_table
+  #' @importFrom xml2 read_html
+  #' @importFrom dplyr filter
+  #' @importFrom dplyr rename
+  #' @importFrom dplyr mutate
+  #' @importFrom dplyr select
+  #' @importFrom purrr map
+  #' @importFrom purrr map_at
+  #' 
+  #' @examples
+  #' polling_data <- germanpolls() 
+  #' 
+  #' @export
+  #'
 
-germanpolls <- function() {
-  
+  germanpolls <- function() {
+    
+  library("purrr")
+  library("rvest")
+  library("XML")
+  library("magrittr")
+  library("dplyr")
+  library("tidyr")
+  library("zoo")
+
+
   # scrape data
   institute <- c("allensbach", "emnid", "forsa", "politbarometer", "gms", "dimap", "insa")
-  parteien <- c("cdu.csu", "spd", "grüne", "fdp", "linke", "afd", "sonstige", "nw_un", "piraten", "fw")
-  
-  
+  parteien <- c("cdu/csu", "spd", "grüne", "fdp", "linke", "afd", "sonstige", "nw_un", "fw")
+  header <- c("CDU/CSU", "SPD", "GRÜNE", "FDP", "LINKE", "AfD", "Sonstige", "Nichtwähler/Unentschl.", "FW", "Befragte", "Zeitraum")
+
   df <-
-    purrr::map_df(institute, function(i) {
-      
-      # keep calm!
+    map_df(institute, function(i) {
+      # keep calm! Fortschrittsbalken
       cat(".")
+      path <- paste0("http://www.wahlrecht.de/umfragen/", i, ".htm")
+      page <- read_html(path, encoding = "utf-8")
       
-      page <- paste0("http://www.wahlrecht.de/umfragen/", i, ".htm") %>% xml2::read_html()
-      data.frame(page %>% rvest::html_table(fill = TRUE), stringsAsFactors=FALSE)
+      rdf <- list()
+      table <- html_node(page, ".wilko")
+      table_head <- html_nodes(table, xpath = "//thead//th") %>% html_text()
+
+      rdf[["datum"]] <- html_nodes(page, xpath = "//table//tbody//tr//td[1]") %>% html_text()
+      rdf[["institut"]] <- rep(i, length = length(rdf[["datum"]]))
+
+      for(head_el in seq_along(header)){
+        vec <- vector()
+        hindex <- match(header[head_el], table_head)
+        # fuck up, extra loop because table of politbarometer is strange structured (td-element instead of th-element)
+        if(i == "politbarometer" && (header[head_el] == "Befragte" || header[head_el] =="Zeitraum")){
+          hindex = hindex + 1
+        }
+        checkvec <- html_nodes(page, xpath = paste0("//table//tbody//tr//td[", hindex,"]")) %>% html_text()
+
+        if (length(checkvec) > 0){
+          vec <- checkvec
+          rdf[[header[head_el]]] <- vec
+        }
+      }
+      as.data.frame(do.call(cbind, as.list(rdf)), stringsAsFactor = F)
     })
-  
-  
+
   # clean data
-  names(df) <- tolower(names(df))
-  
-  df <- df %>%
-    dplyr::filter(grepl("%", cdu.csu)) %>%
-    dplyr::filter(!grepl("Bundestagswahl", befragte)) %>%
-    dplyr::rename(institut = x1, datum2 = datum, nw_un = nichtwähler.unentschl.) %>%
-    dplyr::mutate(datum = ifelse(is.na(x.), datum2, x.)) %>%
-    dplyr::select(institut, datum, befragte, zeitraum, cdu.csu, spd, grüne, fdp, linke, afd, sonstige, nw_un,piraten,fw) %>%
-    dplyr::mutate(typ = ifelse(
+  names(df) %<>% tolower()
+
+  df %<>%
+    # filter(grepl("%", `cdu/csu`)) %>%
+    filter(!grepl("Bundestagswahl", befragte)) %>%
+    rename(nw_un = `nichtwähler/unentschl.`) %>%
+      select(institut, datum, befragte, zeitraum, `cdu/csu`, spd, `grüne`, fdp, linke, afd, sonstige, nw_un,fw) %>%
+    mutate(typ = ifelse(
       grepl("O • ", befragte), "online",
       ifelse(grepl("T • ", befragte), "telefon", "keineangabe")))
-  
+
   # gsub
-  df <- df %>% purrr::map(gsub, pattern = " %|≈|O • |T • |[?]", replacement = "")
-  df <- df %>% purrr::map(gsub, pattern = ",", replacement = ".")
-  df <- df %>% purrr::map_at("befragte", gsub, pattern = "[.]", replacement = "")
-  
+  df %<>% map(gsub, pattern = " %|≈|O • |T • |[?]", replacement = "")
+  df %<>% map(gsub, pattern = ",", replacement = ".")
+  df %<>% map_at("befragte", gsub, pattern = "[.]", replacement = "")
+
   # classes
-  df <- df %>% purrr::map_at(parteien, as.numeric)
-  df <- df %>% purrr::map_at("befragte", as.numeric)
+  df %<>% map_at(parteien, as.numeric)
+  df %<>% map_at("befragte", as.numeric)
   df$datum <- as.Date(df$datum, "%d.%m.%Y")
-  df <- df %>% as.data.frame()
-  
+
+  df %<>% as.data.frame()
+  df <- filter(df, !is.na(df$befragte))
+
+  write.csv(df, file="data/data-input.csv", row.names = F, quote = F)
+
+  # transform data set to longform
+  df_l <- df %>% gather(partei, anteil, -institut, -datum, -befragte, -zeitraum, -typ)
+  df_l$anteil <- df_l$anteil/100
+
   df
-  
-}
+
+
+    
+  }
